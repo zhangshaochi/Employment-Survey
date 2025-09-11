7ffc1185cc7cf25ad98ce2ea39044d97a3e294b5
zhangshaochi <52228907+zhangshaochi@users.noreply.github.com>
2025-09-11 13:27:07 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Application</h1>
+            <p>This is a simple home page.</p>
+        </div>
+    );
+};
+
+export default HomePage;
