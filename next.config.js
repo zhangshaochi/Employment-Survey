e3a38cdf94a94d3ce268de6ceb06f2a797ff3aad
zhangshaochi <52228907+zhangshaochi@users.noreply.github.com>
2025-09-14 09:20:28 +0800

    Update next.config.js
    1

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ module.exports = {
   experimental: {
     outputFileTracingRoot: __dirname,
   },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-      os: false
-    };
-    return config;
-  }
 };
